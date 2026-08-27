ad6c3292dda99cc77289554141371c47d8773641
msantos7gabriel <gabriel.msantos7@hotmail.com>
2026-08-27 20:13:54 -0300

    feat: enhance mission reporting with duration and battery usage metrics

--- src/colibricf/task.py
+++ src/colibricf/task.py
@@ -15,15 +15,28 @@ class Task(ABC):
     """
 
     def __init__(self, gpio: Union[int, None] = None) -> None:
+        # Initializing task components
         self.drone = Drone()
         self.logger = Logger()
         self.logger.start()
         rospy.sleep(3)
 
+        # Initializig mission report variables 
+        self.mission_start_time = rospy.get_rostime().to_sec()
+        self.drone_start_battery = self.drone.get_telemetry().voltage 
+
+
         if gpio is not None:
             self.servo = Servo(gpio)
 
         self.camera = Camera()
+
+    def mission_report(self):
+        """
+        A method to generate a mission report. Useful in most cases.
+        """
+        rospy.loginfo(f"Mission duration: {self.mission_end_time - self.mission_start_time:.2f} seconds")
+        rospy.loginfo(f"Battery used: {self.drone_start_battery - self.drone_end_battery:.2f} V")
 
     @abstractmethod
     def mission(self) -> None:
@@ -48,5 +61,10 @@ class Task(ABC):
         finally:
             self.drone.land_wait()
             self.camera.stop()
+            #Mission report variables
+            self.mission_end_time = rospy.get_rostime().to_sec()
+            self.drone_end_battery = self.drone.get_telemetry().voltage
+            self.mission_report()
+
             rospy.sleep(3)
             self.logger.stop()
